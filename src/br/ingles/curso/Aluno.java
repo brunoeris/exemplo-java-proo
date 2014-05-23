a20dcc31795c022d6adad3ce0919608b00313f77
Otacílio Neto <otaciliolucas@gmail.com>
2014-05-22 21:45:47 -0300

    Implementação do switch

--- src/br/ingles/curso/Aluno.java
+++ src/br/ingles/curso/Aluno.java
@@ -14,6 +14,31 @@ public class Aluno {
 			System.out.println("\nNome: "+this.nome);
 			System.out.println("Matricula: "+this.matricula);	
 		}
-			 
+		
+		public void excluirAluno(){
+			this.nome = " ";
+			this.matricula = 0;
+		}
+
+		//getters and setters
+		
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public int getMatricula() {
+			return matricula;
+		}
+
+		public void setMatricula(int matricula) {
+			this.matricula = matricula;
+		}
 
 }
+		
+
+
--- src/br/ingles/curso/Secretaria.java
+++ src/br/ingles/curso/Secretaria.java
@@ -2,80 +2,135 @@ package br.ingles.curso;
 
 import java.util.Scanner;
 
+
 public class Secretaria {
 	private static Scanner leitor;
 	
 	public static void main(String[]args) {
 		
+		Aluno aluno = new Aluno("",0);
+		
+		Boletim boletim = new Boletim(aluno.nome, aluno.matricula, 0, 0);
+		
+		
 		leitor = new Scanner (System.in);
 		
+		//MENU
+		System.out.println("CURSO DE INGLÊS 'HOW ARE YOU LEARNING?' ");
+		System.out.println("MENU");
+		System.out.println("1- PARA INSERIR ALUNO");
+		System.out.println("2- PARA PROCURAR UM ALUNO");
+		System.out.println("3- PARA INSERIR NOTAS DE UM ALUNO");
+		System.out.println("4- PARA EDITAR A NOTA DE UM ALUNO");
+		System.out.println("3- PARA MOSTRAR O BOLETIM DE UM ALUNO");
+		System.out.println("6- PARA EXCLUIR UM ALUNO");
+		System.out.println("7- PARA FECHAR O PROGORAMA");
+		
+		int opcaoMenu = leitor.nextInt();
+		
+		
+		
+		switch (opcaoMenu){
 		//inserirAluno()
 		
-		System.out.println("Digite o nome: ");
-		String nome = leitor.next();
-		System.out.println("Digite a matricula: ");
-		int matricula = leitor.nextInt();
-		
-		Aluno aluno = new Aluno(nome, matricula);
-		
-		//procurarAluno();
-		
-		System.out.println("Digite o nome do aluno que voce deseja encontrar: ");
-		String procura = leitor.next();
-		
-		if(procura.equals(aluno.nome)){
-			System.out.println("Aluno Encontrado");
-			aluno.imprimir();	
-		}else{
-			System.out.println("Aluno nao encontrado!");
-		}
-		
-		
-		//inserirNotas()
-		
-		System.out.println("Digite a nota da primeira prova: ");
-		float nota1 = leitor.nextFloat();
-		System.out.println("Digite a nota da segunda prova: ");
-		float nota2 = leitor.nextFloat();
-		
-		Boletim boletim = new Boletim(aluno.nome, aluno.matricula, nota1, nota2);
-	
-		//mostrarBoletim
-		
-		boletim.imprimirBoletim();
-		
-		//editarNotas()
-		
-		System.out.println("Digite o nome do aluno que voce deseja editar a nota: ");
-		procura = leitor.next();
-		
-		if(procura.equals(aluno.nome)){
-			System.out.println("Aluno Encontrado");
-			System.out.println("Você deseja editar qual das notas do aluno? Digite");
-			System.out.println("1- A primeira nota \n2-A segunda nota");
-			int escolha = leitor.nextInt();
+			case 1:
 			
-			if(escolha == 1){
-				System.out.println("Digite a nova nota do aluno: ");
-				nota1 = leitor.nextFloat();
-				boletim.setNota1(nota1);
-				boletim.setNotaFinal((nota1+nota2)/2);
-				boletim.imprimirBoletim();
-			}
-			
-			if(escolha == 2){
-				System.out.println("Digite a nova nota do aluno: ");
-				nota2 = leitor.nextFloat();
-				boletim.setNota2(nota2);
-				boletim.setNotaFinal((nota1+nota2)/2);
-				boletim.imprimirBoletim();
-			}
+				System.out.println("Digite o nome: ");
+				String nome = leitor.next();
+				System.out.println("Digite a matricula: ");
+				int matricula = leitor.nextInt();
+				aluno.setNome(nome);
+				aluno.setMatricula(matricula);
 				
-		}else{
-			System.out.println("Aluno nao encontrado!");
-		}
-		
+			break;
 			
-	}
+			case 2:
+			//procurarAluno();
+				
+				
+				System.out.println("Digite o nome do aluno que voce deseja encontrar: ");
+				String procura = leitor.next();
+				
+				if(procura.equals(aluno.nome)){
+					System.out.println("Aluno Encontrado");
+					aluno.imprimir();	
+				}else{
+					System.out.println("Aluno nao encontrado!");
+				}
+			break;
+			
+			case 3:	
+			//inserirNotas()
+			
+				System.out.println("Digite a nota da primeira prova: ");
+				float nota1 = leitor.nextFloat();
+				System.out.println("Digite a nota da segunda prova: ");
+				float nota2 = leitor.nextFloat();
+				boletim.setNota1(nota1);
+				boletim.setNota2(nota2);
+				
+			break;
+			
+			case 4:
+			//editarNotas()
+				System.out.println("Digite o nome do aluno que voce deseja editar a nota: ");
+				procura = leitor.next();
+				
+				if(procura.equals(aluno.nome)){
+					System.out.println("Aluno Encontrado!");
+					System.out.println("Você deseja editar qual das notas do aluno? Digite");
+					System.out.println("1- A primeira nota \n2-A segunda nota");
+					int escolha = leitor.nextInt();
+					
+					if(escolha == 1){
+						System.out.println("Digite a nova nota do aluno: ");
+						nota1 = leitor.nextFloat();
+						boletim.setNota1(nota1);
+						boletim.setNotaFinal((boletim.getNota1()+boletim.getNota2())/2);
+						boletim.imprimirBoletim();
+					}
+					
+					if(escolha == 2){
+						System.out.println("Digite a nova nota do aluno: ");
+						nota2 = leitor.nextFloat();
+						boletim.setNota2(nota2);
+						boletim.setNotaFinal((boletim.getNota1()+boletim.getNota2())/2);
+						boletim.imprimirBoletim();
+					}
+						
+				}else{
+					System.out.println("Aluno nao encontrado!");
+				}
+			break;
+			
+			case 5:
+			//mostrarBoletim
+					boletim.imprimirBoletim();
+			break;
+				
+			/*
+			case 6:
+			//excluirAluno()
+				String procura;
+				
+				if(procura.equals(aluno.nome)){
+					System.out.println("Aluno Encontrado");
+					aluno.imprimir();
+					System.out.println("\nVoce tem certeza de que quer excluir o aluno?");
+					System.out.println("1- SIM\n2- NAO");
+					int escolha = leitor.nextInt();
+					
+					if(escolha == 1){
+						aluno.excluirAluno();
+						aluno.imprimir();
+					}
+					
+				}
+			break;
+			*/
+			
+			}
+			
+		}
 
 }
