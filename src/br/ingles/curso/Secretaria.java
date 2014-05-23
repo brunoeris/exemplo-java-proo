efafc59119773e99921e07dd339e6d1d19594c91
Otacílio Neto <otaciliolucas@gmail.com>
2014-05-22 22:01:49 -0300

    últimos updates, do while

--- src/br/ingles/curso/Secretaria.java
+++ src/br/ingles/curso/Secretaria.java
@@ -15,122 +15,130 @@ public class Secretaria {
 		
 		leitor = new Scanner (System.in);
 		
-		//MENU
-		System.out.println("CURSO DE INGLÊS 'HOW ARE YOU LEARNING?' ");
-		System.out.println("MENU");
-		System.out.println("1- PARA INSERIR ALUNO");
-		System.out.println("2- PARA PROCURAR UM ALUNO");
-		System.out.println("3- PARA INSERIR NOTAS DE UM ALUNO");
-		System.out.println("4- PARA EDITAR A NOTA DE UM ALUNO");
-		System.out.println("3- PARA MOSTRAR O BOLETIM DE UM ALUNO");
-		System.out.println("6- PARA EXCLUIR UM ALUNO");
-		System.out.println("7- PARA FECHAR O PROGORAMA");
+		int opcaoMenu;
 		
-		int opcaoMenu = leitor.nextInt();
+		do{
 		
-		
-		
-		switch (opcaoMenu){
-		//inserirAluno()
-		
-			case 1:
+			//MENU
+			System.out.println("CURSO DE INGLÊS 'HOW ARE YOU LEARNING?' ");
+			System.out.println("MENU");
+			System.out.println("1- PARA INSERIR ALUNO");
+			System.out.println("2- PARA PROCURAR UM ALUNO");
+			System.out.println("3- PARA INSERIR NOTAS DE UM ALUNO");
+			System.out.println("4- PARA EDITAR A NOTA DE UM ALUNO");
+			System.out.println("3- PARA MOSTRAR O BOLETIM DE UM ALUNO");
+			System.out.println("6- PARA EXCLUIR UM ALUNO");
+			System.out.println("7- PARA FECHAR O PROGORAMA");
 			
-				System.out.println("Digite o nome: ");
-				String nome = leitor.next();
-				System.out.println("Digite a matricula: ");
-				int matricula = leitor.nextInt();
-				aluno.setNome(nome);
-				aluno.setMatricula(matricula);
-				
-			break;
+			opcaoMenu = leitor.nextInt();
 			
-			case 2:
-			//procurarAluno();
-				
-				
-				System.out.println("Digite o nome do aluno que voce deseja encontrar: ");
-				String procura = leitor.next();
-				
-				if(procura.equals(aluno.nome)){
-					System.out.println("Aluno Encontrado");
-					aluno.imprimir();	
-				}else{
-					System.out.println("Aluno nao encontrado!");
-				}
-			break;
 			
-			case 3:	
-			//inserirNotas()
 			
-				System.out.println("Digite a nota da primeira prova: ");
-				float nota1 = leitor.nextFloat();
-				System.out.println("Digite a nota da segunda prova: ");
-				float nota2 = leitor.nextFloat();
-				boletim.setNota1(nota1);
-				boletim.setNota2(nota2);
-				
-			break;
+			switch (opcaoMenu){
+			//inserirAluno()
 			
-			case 4:
-			//editarNotas()
-				System.out.println("Digite o nome do aluno que voce deseja editar a nota: ");
-				procura = leitor.next();
+				case 1:
 				
-				if(procura.equals(aluno.nome)){
-					System.out.println("Aluno Encontrado!");
-					System.out.println("Você deseja editar qual das notas do aluno? Digite");
-					System.out.println("1- A primeira nota \n2-A segunda nota");
-					int escolha = leitor.nextInt();
+					System.out.println("Digite o nome: ");
+					String nome = leitor.next();
+					System.out.println("Digite a matricula: ");
+					int matricula = leitor.nextInt();
+					aluno.setNome(nome);
+					aluno.setMatricula(matricula);
 					
-					if(escolha == 1){
-						System.out.println("Digite a nova nota do aluno: ");
-						nota1 = leitor.nextFloat();
-						boletim.setNota1(nota1);
-						boletim.setNotaFinal((boletim.getNota1()+boletim.getNota2())/2);
-						boletim.imprimirBoletim();
-					}
+				break;
+				
+				case 2:
+				//procurarAluno();
 					
-					if(escolha == 2){
-						System.out.println("Digite a nova nota do aluno: ");
-						nota2 = leitor.nextFloat();
-						boletim.setNota2(nota2);
-						boletim.setNotaFinal((boletim.getNota1()+boletim.getNota2())/2);
-						boletim.imprimirBoletim();
+					
+					System.out.println("Digite o nome do aluno que voce deseja encontrar: ");
+					String procura = leitor.next();
+					
+					if(procura.equals(aluno.nome)){
+						System.out.println("Aluno Encontrado");
+						aluno.imprimir();	
+					}else{
+						System.out.println("Aluno nao encontrado!");
 					}
+				break;
+				
+				case 3:	
+				//inserirNotas()
+				
+					System.out.println("Digite a nota da primeira prova: ");
+					float nota1 = leitor.nextFloat();
+					System.out.println("Digite a nota da segunda prova: ");
+					float nota2 = leitor.nextFloat();
+					boletim.setNota1(nota1);
+					boletim.setNota2(nota2);
+					boletim.setNotaFinal((boletim.getNota1()+boletim.getNota2())/2);
+					
+				break;
+				
+				case 4:
+				//editarNotas()
+					System.out.println("Digite o nome do aluno que voce deseja editar a nota: ");
+					procura = leitor.next();
+					
+					if(procura.equals(aluno.nome)){
+						System.out.println("Aluno Encontrado!");
+						System.out.println("Você deseja editar qual das notas do aluno? Digite");
+						System.out.println("1- A primeira nota \n2-A segunda nota");
+						int escolha = leitor.nextInt();
 						
-				}else{
-					System.out.println("Aluno nao encontrado!");
-				}
-			break;
-			
-			case 5:
-			//mostrarBoletim
-					boletim.imprimirBoletim();
-			break;
-				
-			/*
-			case 6:
-			//excluirAluno()
-				String procura;
-				
-				if(procura.equals(aluno.nome)){
-					System.out.println("Aluno Encontrado");
-					aluno.imprimir();
-					System.out.println("\nVoce tem certeza de que quer excluir o aluno?");
-					System.out.println("1- SIM\n2- NAO");
-					int escolha = leitor.nextInt();
-					
-					if(escolha == 1){
-						aluno.excluirAluno();
-						aluno.imprimir();
+						if(escolha == 1){
+							System.out.println("Digite a nova nota do aluno: ");
+							nota1 = leitor.nextFloat();
+							boletim.setNota1(nota1);
+							boletim.setNotaFinal((boletim.getNota1()+boletim.getNota2())/2);
+							boletim.imprimirBoletim();
+						}
+						
+						if(escolha == 2){
+							System.out.println("Digite a nova nota do aluno: ");
+							nota2 = leitor.nextFloat();
+							boletim.setNota2(nota2);
+							boletim.setNotaFinal((boletim.getNota1()+boletim.getNota2())/2);
+							boletim.imprimirBoletim();
+						}
+							
+					}else{
+						System.out.println("Aluno nao encontrado!");
 					}
+				break;
+				
+				case 5:
+				//mostrarBoletim
+						boletim.imprimirBoletim();
+				break;
 					
-				}
-			break;
-			*/
+				/*
+				case 6:
+				//excluirAluno()
+					String procura;
+					
+					if(procura.equals(aluno.nome)){
+						System.out.println("Aluno Encontrado");
+						aluno.imprimir();
+						System.out.println("\nVoce tem certeza de que quer excluir o aluno?");
+						System.out.println("1- SIM\n2- NAO");
+						int escolha = leitor.nextInt();
+						
+						if(escolha == 1){
+							aluno.excluirAluno();
+							aluno.imprimir();
+						}
+						
+					}
+				break;
+				 */
 			
 			}
 			
-		}
+		}while (opcaoMenu != 7);
+		
+		
+	}
 
 }
